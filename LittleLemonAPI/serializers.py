7c946d6180f7a9abf3cdc8e8dd7f6bbd9acbbedb
Leonardo Rodriguez <leonardorm7@hotmail.com>
2023-04-14 07:24:53 -0500

    adding a category field

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -1,7 +1,27 @@
 from rest_framework import serializers
-from .models import MenuItem
+from .models import MenuItem,Category
+from decimal import Decimal
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'slug', 'title']
 
 class MenuItemSerializer(serializers.ModelSerializer):
+    stock= serializers.IntegerField(source="inventory")#to change the name 
+    price_after_tax=serializers.SerializerMethodField(method_name="calculate_tax")# to add a new field
+    category=CategorySerializer()
+    #depth=1
     class Meta:
         model=MenuItem
-        fields=['id','title','price','inventory']
+        fields=['id','title','price','inventory',"stock","price_after_tax","category"]
+      
+    def calculate_tax(self,product:MenuItem):
+        return product.price*Decimal(1.1)
+#------------Si solo se quiere mostrar el id y el title:------------
+# class MenuItemSerializer(serializers.Serializer):
+
+#     id=serializers.IntegerField()
+#     title=serializers.CharField(max_length=255)
+#     price=serializers.DecimalField(max_digits=6, decimal_places=2)
+#     inventory=serializers.IntegerField()
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import MenuItem
 from .serializers import MenuItemSerializer
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 class MenuItemsView(generics.ListCreateAPIView):
