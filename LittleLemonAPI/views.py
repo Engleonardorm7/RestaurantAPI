93b48ff94cd0d17692bbfd123199d57ce61eac88
Leonardo Rodriguez <leonardorm7@hotmail.com>
2023-04-17 22:08:46 -0500

    adding filters and searching options in the class base view project

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -21,51 +21,25 @@ from django.core.paginator import Paginator, EmptyPage
 #     queryset=MenuItem.objects.all()
 #     serializer_class=MenuItemSerializer
 
-@api_view(['GET','POST'])
-def menu_items(request):
-    if request.method=='GET':
-        items = MenuItem.objects.select_related('category').all()
-        category_name=request.query_params.get('category')
-        to_price=request.query_params.get('to_price')
-        search=request.query_params.get('search')
-        ordering=request.query_params.get('ordering')
-        perpage=request.query_params.get('pepage',default=2)
-        page=request.query_params.get('page',default=1)
+from rest_framework.response import Response 
+from rest_framework import viewsets 
+from .models import MenuItem 
+from .serializers import MenuItemSerializer  
 
-        if category_name:
-            items=items.filter(category__title=category_name)
-        if to_price:
-            items=items.filter(price__lte=to_price)
-        if search:
-            items=items.filter(title__startswith=search)
-        if ordering:
-            #items=items.order_by(ordering) para ordenar por un campo
-            ordering_fields=ordering.split(",")
-            items=items.order_by(*ordering_fields)
-        paginator=Paginator(items, per_page=perpage)
-        try:
-            items=paginator.page(number=page)
-        except EmptyPage:
-            items=[]
+class MenuItemsViewSet(viewsets.ModelViewSet):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    ordering_fields=['price','inventory']
+    search_fields=['title','category__title']
 
+# @api_view()
+# def single_item(request, id):
+#     item = get_object_or_404(Category,pk=id)
+#     serialized_item = MenuItemSerializer(item)
+#     return Response(serialized_item.data) 
 
-        serialized_item = MenuItemSerializer(items,many=True)
-        return Response(serialized_item.data) 
-    if request.method=='POST':
-        serialized_item=MenuItemSerializer(data=request.data)
-        serialized_item.is_valid(raise_exception=True)
-        serialized_item.save()
-        return Response(serialized_item.data, status.HTTP_201_CREATED) 
-
-
-@api_view()
-def single_item(request, id):
-    item = get_object_or_404(Category,pk=id)
-    serialized_item = MenuItemSerializer(item)
-    return Response(serialized_item.data) 
-
-@api_view()
-def category_detail(request, pk):
-    category = get_object_or_404(Category,pk=pk)
-    serialized_category = CategorySerializer(category)
-    return Response(serialized_category.data) 
+# @api_view()
+# def category_detail(request, pk):
+#     category = get_object_or_404(Category,pk=pk)
+#     serialized_category = CategorySerializer(category)
+#     return Response(serialized_category.data) 
