422cde8e8d94c982eea313ce9f2e42800d6b22b3
Leonardo Rodriguez <leonardorm7@hotmail.com>
2023-04-17 22:51:45 -0500

    adding filters and searching options in the class base view project

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -23,7 +23,7 @@ from django.core.paginator import Paginator, EmptyPage
 
 from rest_framework.response import Response 
 from rest_framework import viewsets 
-from .models import MenuItem 
+from .models import MenuItem
 from .serializers import MenuItemSerializer  
 
 class MenuItemsViewSet(viewsets.ModelViewSet):
